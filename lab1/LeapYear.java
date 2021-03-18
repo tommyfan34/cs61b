b37640e2ecfdfaf453dd41f17c5a33c796752e28
Xiao Fan <tommyfanzju@gmail.com>
2021-03-18 20:10:19 +0800

    finished lab 1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,15 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author FAN XIAO
  */
 public class LeapYear {
+    /** Determines whether a given @param year 
+     * is a leap year */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        }
+        return false;
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
