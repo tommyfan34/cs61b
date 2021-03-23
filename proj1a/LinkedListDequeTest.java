45cdce2b24e3b3be6ccf090a64fe8fece9f651f8
Xiao Fan <tommyfanzju@gmail.com>
2021-03-23 20:50:09 +0800

    style check

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -1,6 +1,5 @@
 /** Performs some basic linked list tests. */
 public class LinkedListDequeTest {
-	
 	/* Utility method for printing out empty checks. */
 	public static boolean checkEmpty(boolean expected, boolean actual) {
 		if (expected != actual) {
@@ -103,4 +102,4 @@ public class LinkedListDequeTest {
 		addRemoveTest();
 		addGetTest();
 	}
-} 
+}
