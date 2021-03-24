97b1991f5f87a3a1329c1e94bafcf7370f1d6f07
Xiao Fan <tommyfanzju@gmail.com>
2021-03-24 18:28:59 +0800

    proj 1b task1

---
+++ proj1b/ArrayDeque.java
@@ -0,0 +1,156 @@
+import java.text.DecimalFormat;
+
+public class ArrayDeque<T> implements Deque<T>{
+    private T[] items;
+    private int size;  // the size of the array currently available
+    private double usageFactor = 0.25;
+    private int fp;    // front pointer
+    private int ep;    // end pointer
+    private int num;   // current length
+
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 8;
+        fp = -1;
+        ep = -1;
+        num = 0;
+    }
+
+    private int incPointer(int pointer) {
+        if (pointer == size - 1) {
+            pointer = 0;
+        } else {
+            pointer += 1;
+        }
+        return pointer;
+    }
+
+    private int decPointer(int pointer) {
+        if (pointer == 0) {
+            pointer = size - 1;
+        } else {
+            pointer -= 1;
+        }
+        return pointer;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        if (fp < 0) {
+            fp = 0;
+            ep = 0;
+        }
+        fp = decPointer(fp);
+        items[fp] = item;
+        num += 1;
+        if (fp == ep) {
+            resize(size * 2);
+        }
+    }
+
+    @Override
+    public void addLast(T item) {
+        if (fp < 0) {
+            fp = 0;
+            ep = 0;
+        }
+        items[ep] = item;
+        ep = incPointer(ep);
+        num += 1;
+        if (fp == ep) {
+            resize(size * 2);
+        }
+    }
+
+    @Override
+    public T removeFirst() {
+        if (num == 0) {
+            return null;
+        }
+        T ret = get(0);
+        items[fp] = null;
+        num -= 1;
+        if (num == 0) {
+            fp = -1;
+            ep = -1;
+        } else {
+            fp = incPointer(fp);
+        }
+        DecimalFormat df = new DecimalFormat("0.0000");
+        if (size >= 16 && Float.parseFloat(df.format((float) num / size)) < usageFactor) {
+            resize(appropriateSize(num));
+        }
+        return ret;
+    }
+
+    @Override
+    public T removeLast() {
+        if (num == 0) {
+            return null;
+        }
+        T ret = get(num - 1);
+        num -= 1;
+        ep = decPointer(ep);
+        items[ep] = null;
+        if (num == 0) {
+            fp = -1;
+            ep = -1;
+        }
+        DecimalFormat df = new DecimalFormat("0.0000");
+        if (size >= 16 && Float.parseFloat(df.format((float) num / size)) < usageFactor) {
+            resize(appropriateSize(num));
+        }
+        return ret;
+    }
+
+    @Override
+    public int size() {
+        return num;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (num == 0);
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= num || index < 0) {
+            return null;
+        }
+        if (fp <= size - 1 - index) {
+            return items[fp + index];
+        } else {
+            return items[fp + index - size];
+        }
+    }
+
+    @Override
+    public void printDeque() {
+        for (int i = 0; i < num; i++) {
+            System.out.print(get(i));
+            if (i != num - 1) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+
+    private void resize(int newSize) {
+        T[] a = (T[]) new Object[newSize];
+        if (num + fp <= size) {
+            System.arraycopy(items, fp, a, 0, num);
+        } else {
+            System.arraycopy(items, fp, a, 0, size - fp);
+            System.arraycopy(items, 0, a, size - fp, num - size + fp);
+        }
+        fp = 0;
+        ep = num;
+        size = newSize;
+        items = a;
+    }
+
+    private int appropriateSize(int number) {
+        return (number + 1) * 2;
+    }
+}
---
+++ proj1b/LinkedListDeque.java
@@ -0,0 +1,124 @@
+public class LinkedListDeque<T> implements Deque<T>{
+    private class Node {
+        private T item;
+        private Node next;
+        private Node prev;
+
+        Node(T i, Node p, Node n) {
+            item = i;
+            prev = p;
+            next = n;
+        }
+    }
+
+    private int size;
+    private Node sentinel;
+
+    /** Creates an empty linked list deque */
+    public LinkedListDeque() {
+        size = 0;
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+    }
+
+    @Override
+    /** Add an item of type T to the front of the deque */
+    public void addFirst(T item) {
+        Node toAssert = new Node(item, sentinel, sentinel.next);
+        sentinel.next.prev = toAssert;
+        sentinel.next = toAssert;
+        size += 1;
+    }
+
+    @Override
+    /** Add an item of type T to the back of the deque */
+    public void addLast(T item) {
+        Node toAssert = new Node(item, sentinel.prev, sentinel);
+        sentinel.prev.next = toAssert;
+        sentinel.prev = toAssert;
+        size += 1;
+    }
+
+    @Override
+    /** Returns true if deque is empty, false otherwise */
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    @Override
+    /** Returns the number of items in the deque
+     *  Must take constant time */
+    public int size() {
+        return size;
+    }
+
+    @Override
+    /** Prints the items in the deque from first to last, seperated by a space */
+    public void printDeque() {
+        Node ptr = sentinel.next;
+        while (ptr != sentinel) {
+            System.out.print(ptr.item);
+            if (ptr.next != sentinel) {
+                System.out.print(" ");
+            }
+            ptr = ptr.next;
+        }
+        System.out.println();
+    }
+
+    @Override
+    /** Removes and returns the item at the front of the deque.
+     *  If no such item exists, returns null */
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        T ret = sentinel.next.item;
+        sentinel.next.next.prev = sentinel;
+        sentinel.next = sentinel.next.next;
+        size -= 1;
+        return ret;
+    }
+
+    @Override
+    /** Removes and returns the item at the back of the deque.
+     *  If no such item exists returns null*/
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        T ret = sentinel.prev.item;
+        sentinel.prev.prev.next = sentinel;
+        sentinel.prev = sentinel.prev.prev;
+        size -= 1;
+        return ret;
+    }
+
+    @Override
+    /** Gets the item at the given index, where 0 is the front,
+     *  1 is the next item, and so forth.
+     *  If no such item exists, returns null. Must not alter the deque.
+     *  Use iteration
+     */
+    public T get(int index) {
+        Node ptr = sentinel.next;
+        for (int i = 0; i != index; i++) {
+            ptr = ptr.next;
+        }
+        return ptr.item;
+    }
+
+    private Node helperGetRecursive(int index, Node n) {
+        if (index == 0) {
+            return n;
+        } else {
+            return helperGetRecursive(index - 1, n.next);
+        }
+    }
+
+    /** Get that uses recursion */
+    public T getRecursive(int index) {
+        return helperGetRecursive(index, sentinel.next).item;
+    }
+}
